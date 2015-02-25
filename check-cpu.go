41c869846a1b5e9e6972bd65bb9914046b289371
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-02-25 15:08:31 +0900

    fix check-cpu.go

--- check-cpu.go
+++ check-cpu.go
@@ -26,13 +26,13 @@ func main() {
 
 	switch {
 	case usage > float64(crit):
-		fmt.Printf("CheckCPU CRITICAL: %f\n", usage)
+		fmt.Printf("CheckCPU CRITICAL: %.0f%%\n", usage)
 		os.Exit(2)
 	case usage > float64(warn):
-		fmt.Printf("CheckCPU WARNING: %f\n", usage)
+		fmt.Printf("CheckCPU WARNING: %.0f%%\n", usage)
 		os.Exit(1)
 	default:
-		fmt.Printf("CheckCPU OK: %f\n", usage)
+		fmt.Printf("CheckCPU OK: %.0f%%\n", usage)
 		os.Exit(0)
 	}
 }
@@ -50,13 +50,13 @@ func cpuUsage(sleep int) float64 {
 		total_diff += diff_stats[i]
 	}
 
-	return float64(100.0) * (total_diff - diff_stats[3]) / total_diff
+	return 100.0 * (total_diff - diff_stats[3]) / total_diff
 }
 
 func getStats() []float64 {
 	contents, _ := ioutil.ReadFile("/proc/stat")
 	line := strings.Split(string(contents), "\n")[0]
-	stats := strings.Split(line, " ")[2:12]
+	stats := strings.Fields(line)[1:11]
 
 	result := make([]float64, len(stats))
 	for i := range stats {
