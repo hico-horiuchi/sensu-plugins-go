bdb735da25b8957b6791174a5a6ececb0d39af10
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-04-28 01:11:41 +0900

    add handler-delete.go

---
+++ handler/handler-delete.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"./sensu/plugin"
+	"net/http"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	handler := plugin.NewHandler("/etc/sensu/conf.d/handler-delete.json")
+	client := handler.Event.Client
+	check := handler.Event.Check
+	config := handler.Config
+
+	status := config.GetPath("delete", "status").MustInt()
+	contain := contains(client.Subscriptions, config.GetPath("delete", "subscriptions").MustArray())
+	if check.Name != "keepalive" || check.Status != status || !contain {
+		return
+	}
+
+	host := config.GetPath("delete", "host").MustString()
+	port := config.GetPath("delete", "port").MustInt()
+	url := "http://" + host + ":" + strconv.Itoa(port) + "/clients/" + client.Name
+
+	user := config.GetPath("delete", "user").MustString()
+	password := config.GetPath("delete", "password").MustString()
+	request, _ := http.NewRequest("DELETE", url, nil)
+	if user != "" && password != "" {
+		request.SetBasicAuth(user, password)
+	}
+
+	http.DefaultClient.Do(request)
+}
+
+func contains(list []string, keys []interface{}) bool {
+	sort.Strings(list)
+
+	for _, v := range keys {
+		key := v.(string)
+		i := sort.SearchStrings(list, key)
+
+		if i < len(list) && list[i] == key {
+			return true
+		}
+	}
+
+	return false
+}
