feba02fd0d1a645e35507b30b49a666ea8bc3e14
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-04-27 23:25:23 +0900

    add check-ping.go

---
+++ check/check-ping.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	var (
+		host    string
+		port    int
+		timeout int64
+	)
+
+	pflag.StringVarP(&host, "host", "h", "localhost", "HOST")
+	pflag.IntVarP(&port, "port", "p", 80, "PORT")
+	pflag.Int64VarP(&timeout, "timeout", "T", 1, "TIMEOUT")
+	pflag.Parse()
+
+	address := host + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout("tcp", address, time.Duration(timeout)*time.Second)
+
+	if err != nil {
+		fmt.Println("CheckPing CRITICAL:", err)
+		os.Exit(2)
+	}
+
+	defer conn.Close()
+	fmt.Printf("CheckPing OK: %s\n", address)
+}
