e978a8e976e21870ac5b4530a8f133eae498a23d
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-08-24 18:16:34 +0900

    print every ports in metrics-snmp.go

--- metrics/metrics-snmp.go
+++ metrics/metrics-snmp.go
@@ -28,6 +28,8 @@ func main() {
 		wg.Add(1)
 
 		go func(host string) {
+			var port string
+
 			beforeTraffics, err := snmpWalk(host, community)
 			if err != nil {
 				wg.Done()
@@ -43,8 +45,12 @@ func main() {
 			}
 
 			tmp := metrics.New("").Hostname(host)
-			tmp.Scheme("snmp.rx_bytes").Print(float64(afterTraffics[0] - beforeTraffics[0]))
-			tmp.Scheme("snmp.tx_bytes").Print(float64(afterTraffics[1] - beforeTraffics[1]))
+			for i := range beforeTraffics[0] {
+				port = strconv.Itoa(i)
+				tmp.Scheme("snmp.rx_bytes." + port).Print(float64(afterTraffics[0][i] - beforeTraffics[0][i]))
+				tmp.Scheme("snmp.tx_bytes." + port).Print(float64(afterTraffics[1][i] - beforeTraffics[1][i]))
+			}
+
 			wg.Done()
 		}(host)
 	}
@@ -52,23 +58,24 @@ func main() {
 	wg.Wait()
 }
 
-func snmpWalk(host string, community string) ([]int64, error) {
+func snmpWalk(host string, community string) ([2][]int64, error) {
 	var traffic int64
-	traffics := make([]int64, 2)
+	var traffics [2][]int64
 
 	for i, oid := range []string{"1.3.6.1.2.1.2.2.1.10", "1.3.6.1.2.1.2.2.1.16"} {
 		out, err := exec.Command("snmpwalk", "-v", "2c", "-c", community, host, oid).Output()
 		if err != nil {
 			return traffics, err
 		}
-		lines := strings.Split(strings.TrimRight(string(out), "\n"), "\n")
 
+		lines := strings.Split(strings.TrimRight(string(out), "\n"), "\n")
 		for _, line := range lines {
 			traffic, err = strconv.ParseInt(strings.Fields(line)[3], 10, 64)
 			if err != nil {
 				return traffics, err
 			}
-			traffics[i] += traffic
+
+			traffics[i] = append(traffics[i], traffic)
 		}
 	}
 
